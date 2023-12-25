601a1554b0ce2cf5b4f47ece076310a49997be30
PeterOstr <61982891+PeterOstr@users.noreply.github.com>
2023-12-25 22:16:35 +0300

    added minor fixes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 from pydantic import BaseModel
+from typing import List
 from fastapi.staticfiles import StaticFiles
+from sklearn.metrics import f1_score, roc_auc_score
 
 import joblib
 
@@ -11,21 +13,47 @@ app = FastAPI()
 class Data_transfer(BaseModel):
     data_sample: str
 
+class CombinedData(BaseModel):
+    gender: List
+    age: List
+    hypertension: List
+    heart_disease: List
+    smoking_history: List
+    bmi: List
+    HbA1c_level: List
+    blood_glucose_level: List
+
 model = joblib.load('xgb_optuna_model.joblib')
 
 
 
 
-# root definiton like / is not neseccary
+# root definiton like '/' is not neseccary
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/model/predict")
-async def predict(data: Data_transfer):
-    data = pd.read_json(data.data_sample, orient='split')
+async def predict(data: CombinedData):
+    data = pd.read_json(data.model_dump_json(), orient='list')
     model_prediction = pd.DataFrame(np.round(model.predict(data)))
     model_prediction = model_prediction.to_json()
     return model_prediction
+
+@app.post("/model/combined")
+async def combined(data: CombinedData):
+    predict_data_df = pd.read_json(data.predict_data.data_sample, orient='list')
+    eval_data_df = pd.read_json(data.eval_data.data_sample, orient='list')
+
+    if predict_data_df.shape[0] != eval_data_df.shape[0]:
+        raise HTTPException(status_code=400, detail="Prediction and evaluation data have different lengths.")
+
+    # Make predictions
+    predictions = model.predict(predict_data_df)
+
+    # Evaluate the model
+    f1, roc_auc = evaluate_model(eval_data_df, target_df)
+
+    return {"f1_score": f1, "roc_auc": roc_auc}
 
 #%%
